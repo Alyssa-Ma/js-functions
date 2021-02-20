76d51a2fed2e045abd0986f1c84e4438518dae55
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-02-19 19:47:34 -0500

    added comments

--- functions/filter.js
+++ functions/filter.js
@@ -1,19 +1,19 @@
 Array.prototype.myFilter = function(callback){
-    let arr = [];
+    let arr = [];   //create new arr and set it to original's length
     arr.length = this.length;
 
     let index = 0
     for(let i = 0; i < this.length; i++){
-        if(callback(this[i], i, this) === true){
+        if(callback(this[i], i, this) === true){    //if the callback is true then we add it to arr
             arr[index] = this[i];
             index++;
         }
     }
-    arr.length = index;
+    arr.length = index; //adjust length
     return arr;
 }
 
-
+//test
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
 const result = words.myFilter(word => word.length > 6);
