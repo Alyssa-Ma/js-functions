1dfa4b9784c078d4e8f938819f58ffaa91da9970
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-02-19 19:07:30 -0500

    added testing

--- functions/indexOf.js
+++ functions/indexOf.js
@@ -1,10 +1,15 @@
 Array.prototype.myIndexOf = function(item){
 
-    let index = -1;
-
     for(let i = 0; i <= this.length; i++)
     {
-        
+        if(this[i] === item)
+            return i;
     }
 
+    return -1;
 }
+
+let arr = [1, 2, 3, 6, 5];
+
+console.log(arr);
+console.log(arr.myIndexOf(8));
