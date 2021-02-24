e49a679d457903254d9b59effd20ae641aedd4cf
Joannef <joannef3188@gmail.com>
2021-02-24 01:34:59 -0500

    implemented objectKeys

---
+++ functions/objectKeys.js
@@ -0,0 +1,29 @@
+/*
+Object.keys()
+
+Without using the native “Object.keys()” method of JavaScript, compose a function titled “grabKeys” that will take in an object
+and return all of the keys of the key:value pairs of that object.
+
+Object.keys() returns an array whose elements are strings corresponding to the enumerable properties found directly upon object.
+The ordering of the properties is the same as that given by looping over the properties of the object manually.
+
+Object.keys() returns An array of strings that represent all the enumerable properties of the given object.
+*/
+Object.grabKeys = function(obj) {
+    let keysArr = [];
+    for(const keys in obj) {
+        keysArr.push(keys);
+    }
+    return keysArr;
+}
+
+
+// TESTING
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+  console.log(Object.grabKeys(object1));
+  // expected output: Array ["a", "b", "c"]
